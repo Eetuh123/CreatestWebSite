be55ec1d8b8e0c011df7ddf07b4fb77d28656925
vagrant <vagrant@homestead>
2023-10-09 22:25:00 +0000

    half of the back body done

--- resources/js/Components/BodyParts/BackBody.vue
+++ resources/js/Components/BodyParts/BackBody.vue
@@ -1,30 +1,94 @@
 <template>
-    <div class="parent-container">
-        <div class="glutes">
-            <GluteL />
-            <GluteR />
-        </div>
-
-        <div class="hamstrings">
-            <HamstringL />
-            <HamstringR />
+    <div class="body">
+        <div class="bodyPart">
+            <div class="bodyPartM scale-50">
+                <SpinalErector />
+                <div class="bodyPartL mt-[-52px] ml-[-100px]">
+                    <LatL />
+                </div>
+                <div class="bodyPartL mt-[-52px] ml-[-50px]">
+                    <GluteL />
+                    <div class="bodyPartL mt-[-12px] ml-[56px]">
+                        <BackAdductorL />
+                    </div>
+                    <div class="bodyPartR mt-[-12px] ml-[56px]">
+                        <GluteMediusL />
+                    </div>
+                    <div class="bodyPartL mt-[-82px] ml-[-23px]">
+                        <HamstringL />
+                        <div class="bodyPartL mt-[-68px] ml-[-33px]">
+                            <BackCalvesL />
+                            <div class="bodyPartL mt-[-50px] ml-[-8px]">
+                                <BackLegL />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="bodyPartR mt-[-52px] ml-[100px]">
+                    <LatR />
+                </div>
+                <div class="bodyPartR mt-[-52px] ml-[70px]">
+                    <GluteR />
+                    <div class="bodyPartR mt-[-12px] ml-[9px]">
+                        <BackAdductorR />
+                    </div>
+                    <div class="bodyPartR mt-[-12px] ml-[9px]">
+                        <GluteMediusR />
+                    </div>
+                    <div class="bodyPartR mt-[-82px] ml-[42px]">
+                        <HamstringR />
+                        <div class="bodyPartR mt-[-68px] ml-[36px]">
+                            <BackCalvesR />
+                            <div class="bodyPartR mt-[-50px] ml-[21px]">
+                                <BackLegR />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
+import LatL from "./Back/LatL.vue";
+import LatR from "./Back/LatR.vue";
+import BackObligueL from "./Back/BackObligueL.vue";
+import BackObligueR from "./Back/BackObligueR.vue";
+import SpinalErector from "./Back/SpinalErector.vue";
+import GluteMediusL from "./Back/GluteMediusL.vue";
+import GluteMediusR from "./Back/GluteMediusR.vue";
 import GluteL from "./Back/GluteL.vue";
 import GluteR from "./Back/GluteR.vue";
 import HamstringL from "./Back/HamstringL.vue";
 import HamstringR from "./Back/HamstringR.vue";
+import BackCalvesL from "./Back/BackCalvesL.vue";
+import BackCalvesR from "./Back/BackCalvesR.vue";
+import BackLegL from "./Back/BackLegL.vue";
+import BackLegR from "./Back/BackLegR.vue";
+import BackAdductorL from "./Back/BackAdductorL.vue";
+import BackAdductorR from "./Back/BackAdductorR.vue";
 
 export default {
     name: "BackBody",
     components: {
+        LatL,
+        LatR,
+        BackObligueL,
+        BackObligueR,
+        SpinalErector,
+        GluteMediusL,
+        GluteMediusR,
         GluteL,
         GluteR,
+        BackAdductorL,
+        BackAdductorR,
         HamstringL,
         HamstringR,
+        BackCalvesL,
+        BackCalvesR,
+        BackLegL,
+        BackLegR,
     },
     data() {
         return {
@@ -51,29 +115,23 @@ export default {
 };
 </script>
 <style>
-.parent-container {
+.body {
     display: flex;
-    flex-direction: column;
+    justify-content: center;
     align-items: center;
+    position: relative;
 }
 
-.glutes,
-.hamstrings {
-    display: flex;
-    justify-content: flex-start;
+.bodyPartM,
+.bodyPartL,
+.bodyPartR {
+    position: absolute;
 }
-
-.glutes > * {
-    margin-right: -5px;
+.bodyPartL,
+.bodyPartR {
 }
-.hamstrings > * {
-    margin-right: +20px;
+.bodyPartR {
 }
-.glutes {
-    margin-bottom: -10;
-}
-
-.hamstrings {
-    margin-top: 0;
+.bodyPartL {
 }
 </style>
