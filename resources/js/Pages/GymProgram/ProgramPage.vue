a874005d7dc1d37a9bea432bb47a26c2748dcee7
vagrant <eetuh25@gmail.com>
2023-11-27 23:49:05 +0000

    ProgramPage updates

--- resources/js/Pages/GymProgram/ProgramPage.vue
+++ resources/js/Pages/GymProgram/ProgramPage.vue
@@ -1,15 +1,23 @@
 <template>
   <div class="flex h-screen">
-
     <!-- Sidebar -->
     <div class="w-1/3 bg-gray-100 p-5 space-y-4 m-4">
+    <div>
       <p v-if="user">{{ user.name }}</p>
         <button class="mb-4 text-blue-600 hover:text-blue-700 focus:outline-none" @click="logout">Logout</button>
+    </div>
       <div class="text-lg">Gym Program name</div>
       <div class="text-sm">Days name + automatic/hardcoded day</div>
       <!-- Exercise list goes here -->
     </div>
-
+    <div class="w-1/3 bg-gray-100 p-5 space-y-4 m-4"></div>
+    <div class="w-1/3 bg-gray-100 p-5 space-y-4 m-4">
+      <div class="grid grid-cols-7 gap-2">
+        <div class="day" v-for="day in daysOfMonth" :key="day">
+          {{ day }}
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -23,6 +31,13 @@ import { Inertia } from "@inertiajs/inertia";
 
 const { props } = usePage();
 const { user, gymPrograms } = toRefs(props.value);
+const currentMonth = ref(new Date().getMonth());
+const currentYear = ref(new Date().getFullYear());
+
+const daysOfMonth = computed(() => {
+  const daysInMonth = new Date(currentYear.value, currentMonth.value + 1, 0).getDate();
+  return Array.from({ length: daysInMonth }, (_, i) => i + 1);
+});
 
 console.log("Full Props Value:", props.value);
 
