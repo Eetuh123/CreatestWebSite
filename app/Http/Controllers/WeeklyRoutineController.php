21764f34d12d9dce6b0a7cf78e6c4a0bb84347ee
vagrant <eetuh25@gmail.com>
2023-12-01 01:39:54 +0000

    controllers

--- app/Http/Controllers/WeeklyRoutineController.php
+++ app/Http/Controllers/WeeklyRoutineController.php
@@ -1,16 +1,13 @@
 <?php
- 
+
 namespace App\Http\Controllers;
- 
+
 use Inertia\Inertia;
 use Inertia\Response;
-use App\Models\GymProgram;
-use App\Models\TrainingBlock;
 use App\Models\WeeklyRoutine;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\RedirectResponse;
- 
+
 class WeeklyRoutineController extends Controller
 {
     /**
@@ -18,21 +15,19 @@ class WeeklyRoutineController extends Controller
      */
     public function index(Request $request): Response
     {
-        return Inertia::render('GymProgram/Index', [
-
-        ]);
+        return Inertia::render('GymProgram/Index', []);
     }
 
     public function store(Request $request): RedirectResponse
     {
 
         $validated = $request->validate([
-            'training_block_id' => 'required|integer', 
-            'week_name' => 'required|string|max:255', 
+            'training_block_id' => 'required|integer',
+            'week_name' => 'required|string|max:255',
             'occurrence_weeks' => 'required|integer|max:52',
         ]);
-        
-      $weeklyRoutine = WeeklyRoutine::create([
+
+        $weeklyRoutine = WeeklyRoutine::create([
             'training_block_id' => $validated['training_block_id'],
             'name' => $validated['week_name'],
             'occurrence_weeks' => $validated['occurrence_weeks'],
