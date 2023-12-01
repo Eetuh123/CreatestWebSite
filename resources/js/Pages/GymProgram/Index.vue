6999bc7e3ee8dcd2c45e1ec37867d284a69d73cf
vagrant <eetuh25@gmail.com>
2023-12-01 01:36:18 +0000

    prettier and removed unecesary files

--- resources/js/Pages/GymProgram/Index.vue
+++ resources/js/Pages/GymProgram/Index.vue
@@ -3,43 +3,43 @@
         <CreateProgram />
     </div>
     <div class="bg-rose-500 h-16 w-32 m-4">
-    <p v-if="user">User Name: {{ user.name }}</p>
-    <button @click="logout">Logout</button>
+        <p v-if="user">User Name: {{ user.name }}</p>
+        <button @click="logout">Logout</button>
     </div>
-    <div v-for="(program) in gymPrograms" :key="program.id" @click="openProgramPage(program.id)"> 
+    <div
+        v-for="program in gymPrograms"
+        :key="program.id"
+        @click="openProgramPage(program.id)"
+    >
         <div class="bg-sky-500 h-32 w-32 m-4">
-        {{ program.name }} 
-        </div>    
+            {{ program.name }}
+        </div>
     </div>
-    
 </template>
 
 <script setup>
 import { toRefs, ref, reactive, onMounted, computed } from "vue";
+import { usePage } from "@inertiajs/inertia-vue3";
+import { Inertia } from "@inertiajs/inertia";
 import axios from "axios";
 import BackBody from "../../Components/BodyParts/BackBody.vue";
 import FrontBody from "../../Components/BodyParts/FrontBody.vue";
 import CreateProgram from "./CreateProgram.vue";
-import { usePage } from "@inertiajs/inertia-vue3";
-import { Inertia } from "@inertiajs/inertia";
 
 const { props } = usePage();
 const { user, gymPrograms } = toRefs(props.value);
 const creationButtonPosition = ref(0);
 
 const handleProgramClick = (index) => {
-  creationButtonPosition.value = (index + 1) % gymPrograms.value.length;
+    creationButtonPosition.value = (index + 1) % gymPrograms.value.length;
 };
 
-const openProgramPage = (programId => {
-     Inertia.visit(route('program.show', { id: programId }));
-});
-
-console.log("Full Props Value:", props.value);
+const openProgramPage = (programId) => {
+    Inertia.visit(route("program.show", { id: programId }));
+};
 
 const logout = () => {
-    Inertia.post('/logout'
-    );
+    Inertia.post("/logout");
 };
 </script>
 <style>
